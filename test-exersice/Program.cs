dbd12242f72cc2a873bd8254e1905ceda955ff73
Chekabell <nikita.toporischev@mail.ru>
2026-08-25 14:00:27 +0500

    Task 5 is completed

--- test-exersice/Program.cs
+++ test-exersice/Program.cs
@@ -1,1 +1,22 @@
-﻿Console.WriteLine("Hello, World!");
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        long sum = 0;
+
+        Console.Write("Enter numbers: ");
+        while (true) 
+        {
+            string? input = Console.ReadLine();
+            if (string.IsNullOrEmpty(input))
+            {
+                break;
+            }
+            if (long.TryParse(input, out long number)) {
+                sum += number;
+            }
+        }
+
+        Console.WriteLine("Sum of numbers = " + sum);
+    }
+}
